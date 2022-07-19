ba52cd785f0b64a954b45a95c69df9f5af55f48e
rafabcanedo <rafaitecanedo@gmail.com>
2022-07-18 21:13:34 -0300

    tela login

--- App.tsx
+++ App.tsx
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { THEME } from './src/styles/theme';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+// Import Components/Screens
+//import { SignIn } from "./src/screens/SignIn";
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+ const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ return (
+  <NativeBaseProvider theme={THEME}>
+    <StatusBar
+     barStyle="light-content"
+     backgroundColor="transparent"
+     translucent
+    />
+   {fontsLoaded ? <Home /> : <Loading />}
+  </NativeBaseProvider>
+ );
+}
